9ffacf980bfaf19a43d8ed6cd70d2127aec8a89d
Laouen <laouen.belloli@gmail.com>
2018-05-24 19:53:51 -0300

    minor refactor and started < operator for real numbers

--- main.cpp
+++ main.cpp
@@ -4,15 +4,15 @@
 
 int main() {
 
-    boost::real::real a(15);
+    boost::real::real a;
     a.add_number<boost::real::third_part>();
-    boost::real::real c(15, {9,9,9,9,9,9});
-    boost::real::real d(15, {9,9,9,9,9,9});
+    boost::real::real c({9,9,9,9,9,9});
+    boost::real::real d({9,9,9,9,9,9});
     boost::real::real e(c + d);
 
-    a.print();
-    c.print();
-    d.print();
+    a.print(3);
+    c.print(3);
+    d.print(3);
 
     boost::real::real::iterator it = e.begin();
     for(int i = 0; i < 10; ++i) {

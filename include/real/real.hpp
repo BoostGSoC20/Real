5e5987b8e5739649e1c81b767d688154ac2b4523
Laouen <laouen.belloli@gmail.com>
2018-06-06 14:12:23 -0300

    refactored static method outside the class in a helper file and namespace

--- include/real/real.hpp
+++ include/real/real.hpp
@@ -9,6 +9,7 @@
 
 #include <real/number.hpp>
 #include <real/real_exception.hpp>
+#include <real/real_helpers.hpp>
 
 namespace boost {
     namespace real {
@@ -38,19 +39,6 @@ namespace boost {
                 this->_precision = std::max(lhs._precision, rhs._precision);
             };
 
-            bool is_lower(const std::list<int>& lhs, std::list<int>& rhs) const {
-
-                // Check if lhs is lower than rhs
-                auto lhs_it = lhs.cbegin();
-                auto rhs_it = rhs.cbegin();
-                while (rhs_it != rhs.end() && lhs_it != lhs.end() && *lhs_it == *rhs_it) {
-                    ++lhs_it;
-                    ++rhs_it;
-                }
-
-                return rhs_it != rhs.end() && lhs_it != lhs.end() && *lhs_it < *rhs_it;
-            }
-
             void copy_operands(const real& other) {
                 if (other._lhs_ptr != nullptr) {
                     this->_lhs_ptr = new real(*other._lhs_ptr);
@@ -65,109 +53,22 @@ namespace boost {
 
             class iterator {
             private:
+
+                // Number range boundaries
                 std::list<int> lower_bound = {};
                 std::list<int> upper_bound = {};
-                int n = 0;
                 int lower_integer_part = 0;
                 int upper_integer_part = 0;
 
+                // Iterator precision
+                int n = 0;
+
+                // Internal number to iterate
                 real* real_ptr = nullptr;
+
+                // If the number is a composition, the iterator uses the operand iterators
                 iterator* lhs_it_ptr = nullptr;
                 iterator* rhs_it_ptr = nullptr;
-
-                void align_numbers(std::list<int>& lhs, int& lhs_integers, std::list<int>& rhs, int& rhs_integers) const {
-                    // This is to align numbers
-                    while (lhs_integers < rhs_integers) {
-                        lhs.push_front(0);
-                        lhs_integers++;
-                    }
-
-                    while (rhs_integers < lhs_integers) {
-                        lhs.push_front(0);
-                        rhs_integers++;
-                    }
-
-                    while (lhs.size() < rhs.size()) {
-                        lhs.push_back(0);
-                    }
-
-                    while (rhs.size() < lhs.size()) {
-                        rhs.push_back(0);
-                    }
-                };
-
-                int add_bounds(std::list<int> lhs, int lhs_integers, std::list<int> rhs, int rhs_integers, std::list<int>& result) const {
-                    int carry = 0;
-                    int digit;
-
-                    this->align_numbers(lhs, lhs_integers, rhs, rhs_integers);
-
-                    auto lhs_it = lhs.crbegin();
-                    auto rhs_it = rhs.crbegin();
-
-                    while(lhs_it != lhs.crend() and rhs_it != rhs.crend()) {
-
-                        digit = carry + *lhs_it + *rhs_it;
-
-                        if (digit > 9) {
-                            carry = 1;
-                            digit -= 10;
-                        } else {
-                            carry = 0;
-                        }
-
-                        result.push_front(digit);
-                        ++lhs_it;
-                        ++rhs_it;
-                    }
-
-                    if (carry == 1) {
-                        result.push_front(1);
-                        return rhs_integers + 1;
-                    }
-
-                    return rhs_integers;
-                };
-
-                /*
-                 * Pre-condition: lhs >= rhs
-                 */
-                int subtract_bounds(std::list<int> lhs, int lhs_integers, std::list<int> rhs, int rhs_integers, std::list<int>& result) const {
-                    int borrow = 0;
-
-                    this->align_numbers(lhs, lhs_integers, rhs, rhs_integers);
-
-                    auto lhs_it = lhs.rbegin();
-                    auto rhs_it = rhs.rbegin();
-
-                    while(lhs_it != lhs.rend() and rhs_it != rhs.rend()) {
-
-                        if (*lhs_it < borrow) {
-                            *lhs_it += (10 - borrow); // Borrow is always 0 or 1, then it is never greater than 10
-                        } else {
-                            *lhs_it -= borrow;
-                            borrow = 0;
-                        }
-
-                        if (*lhs_it < *rhs_it) {
-                            *lhs_it += 10;
-                            borrow++;
-                        }
-
-                        result.push_front(*lhs_it - *rhs_it);
-                        ++lhs_it;
-                        ++rhs_it;
-                    }
-
-
-                    // Remove possible 0 prefix if more significant digits were canceled.
-                    while (result.front() == 0 && lhs_integers > 0) {
-                        result.pop_front();
-                        --lhs_integers;
-                    }
-
-                    return lhs_integers;
-                };
 
             public:
                 iterator() = default;
@@ -245,11 +146,11 @@ namespace boost {
                     // TODO: The subtraction assumes lhs >= rhs, otherwise fails.
                     // TODO: positive-negative leq and geq number combination must be considered to get correct result for all the cases.
                     if (this->real_ptr->_operation == OP::ADDITION) {
-                        this->lower_integer_part = this->add_bounds((*this->lhs_it_ptr).get_lower_bound(), (*this->lhs_it_ptr).lower_integer_part, (*this->rhs_it_ptr).get_lower_bound(), (*this->rhs_it_ptr).lower_integer_part, this->lower_bound);
-                        this->upper_integer_part = this->add_bounds((*this->lhs_it_ptr).get_upper_bound(), (*this->lhs_it_ptr).upper_integer_part, (*this->rhs_it_ptr).get_upper_bound(), (*this->rhs_it_ptr).upper_integer_part, this->upper_bound);
+                        this->lower_integer_part = boost::real::helper::add_bounds((*this->lhs_it_ptr).get_lower_bound(), (*this->lhs_it_ptr).lower_integer_part, (*this->rhs_it_ptr).get_lower_bound(), (*this->rhs_it_ptr).lower_integer_part, this->lower_bound);
+                        this->upper_integer_part = boost::real::helper::add_bounds((*this->lhs_it_ptr).get_upper_bound(), (*this->lhs_it_ptr).upper_integer_part, (*this->rhs_it_ptr).get_upper_bound(), (*this->rhs_it_ptr).upper_integer_part, this->upper_bound);
                     } else if (this->real_ptr->_operation == OP::SUBTRACT) {
-                        this->lower_integer_part = this->subtract_bounds((*this->lhs_it_ptr).get_lower_bound(), (*this->lhs_it_ptr).lower_integer_part, (*this->rhs_it_ptr).get_upper_bound(), (*this->rhs_it_ptr).lower_integer_part, this->lower_bound);
-                        this->upper_integer_part = this->subtract_bounds((*this->lhs_it_ptr).get_upper_bound(), (*this->lhs_it_ptr).upper_integer_part, (*this->rhs_it_ptr).get_lower_bound(), (*this->rhs_it_ptr).upper_integer_part, this->upper_bound);
+                        this->lower_integer_part = boost::real::helper::subtract_bounds((*this->lhs_it_ptr).get_lower_bound(), (*this->lhs_it_ptr).lower_integer_part, (*this->rhs_it_ptr).get_upper_bound(), (*this->rhs_it_ptr).lower_integer_part, this->lower_bound);
+                        this->upper_integer_part = boost::real::helper::subtract_bounds((*this->lhs_it_ptr).get_upper_bound(), (*this->lhs_it_ptr).upper_integer_part, (*this->rhs_it_ptr).get_lower_bound(), (*this->rhs_it_ptr).upper_integer_part, this->upper_bound);
                     }
                 }
 
@@ -334,6 +235,8 @@ namespace boost {
 
             iterator begin() { return iterator(this); }
 
+            /************** Operators ******************/
+
             real operator+(const real& other) const {
                 return real(real::OP::ADDITION, *this, other);
             }
@@ -363,11 +266,11 @@ namespace boost {
                     ++this_it;
                     ++other_it;
 
-                    if (this->is_lower(this_it.get_upper_bound(), other_it.get_lower_bound())) {
+                    if (boost::real::helper::is_lower(this_it.get_upper_bound(), other_it.get_lower_bound())) {
                         return true;
                     }
 
-                    if (this->is_lower(other_it.get_upper_bound(), this_it.get_lower_bound())) {
+                    if (boost::real::helper::is_lower(other_it.get_upper_bound(), this_it.get_lower_bound())) {
                         return false;
                     }
                 }
---
+++ include/real/real_helpers.hpp
@@ -0,0 +1,122 @@
+#ifndef BOOST_REAL_REAL_HELPERS_HPP
+#define BOOST_REAL_REAL_HELPERS_HPP
+
+#include <list>
+
+namespace boost {
+    namespace real {
+        namespace helper {
+
+            bool is_lower(const std::list<int>& lhs, const std::list<int>& rhs) {
+
+                // Check if lhs is lower than rhs
+                auto lhs_it = lhs.cbegin();
+                auto rhs_it = rhs.cbegin();
+                while (rhs_it != rhs.end() && lhs_it != lhs.end() && *lhs_it == *rhs_it) {
+                    ++lhs_it;
+                    ++rhs_it;
+                }
+
+                return rhs_it != rhs.end() && lhs_it != lhs.end() && *lhs_it < *rhs_it;
+            }
+
+            /*
+             * Align two numbers so the digits positions corresponds
+             */
+            void align_numbers(std::list<int>& lhs, int& lhs_integers, std::list<int>& rhs, int& rhs_integers) {
+                while (lhs_integers < rhs_integers) {
+                    lhs.push_front(0);
+                    lhs_integers++;
+                }
+
+                while (rhs_integers < lhs_integers) {
+                    lhs.push_front(0);
+                    rhs_integers++;
+                }
+
+                while (lhs.size() < rhs.size()) {
+                    lhs.push_back(0);
+                }
+
+                while (rhs.size() < lhs.size()) {
+                    rhs.push_back(0);
+                }
+            }
+
+            int add_bounds(std::list<int> lhs, int lhs_integers, std::list<int> rhs, int rhs_integers, std::list<int>& result) {
+                int carry = 0;
+                int digit;
+
+                boost::real::helper::align_numbers(lhs, lhs_integers, rhs, rhs_integers);
+
+                auto lhs_it = lhs.crbegin();
+                auto rhs_it = rhs.crbegin();
+
+                while(lhs_it != lhs.crend() and rhs_it != rhs.crend()) {
+
+                    digit = carry + *lhs_it + *rhs_it;
+
+                    if (digit > 9) {
+                        carry = 1;
+                        digit -= 10;
+                    } else {
+                        carry = 0;
+                    }
+
+                    result.push_front(digit);
+                    ++lhs_it;
+                    ++rhs_it;
+                }
+
+                if (carry == 1) {
+                    result.push_front(1);
+                    return rhs_integers + 1;
+                }
+
+                return rhs_integers;
+            }
+
+            /*
+             * Pre-condition: lhs >= rhs
+             */
+            int subtract_bounds(std::list<int> lhs, int lhs_integers, std::list<int> rhs, int rhs_integers, std::list<int>& result) {
+                int borrow = 0;
+
+                boost::real::helper::align_numbers(lhs, lhs_integers, rhs, rhs_integers);
+
+                auto lhs_it = lhs.rbegin();
+                auto rhs_it = rhs.rbegin();
+
+                while(lhs_it != lhs.rend() and rhs_it != rhs.rend()) {
+
+                    if (*lhs_it < borrow) {
+                        *lhs_it += (10 - borrow); // Borrow is always 0 or 1, then it is never greater than 10
+                    } else {
+                        *lhs_it -= borrow;
+                        borrow = 0;
+                    }
+
+                    if (*lhs_it < *rhs_it) {
+                        *lhs_it += 10;
+                        borrow++;
+                    }
+
+                    result.push_front(*lhs_it - *rhs_it);
+                    ++lhs_it;
+                    ++rhs_it;
+                }
+
+
+                // Remove possible 0 prefix if more significant digits were canceled.
+                while (result.front() == 0 && lhs_integers > 0) {
+                    result.pop_front();
+                    --lhs_integers;
+                }
+
+                return lhs_integers;
+            }
+        }
+    }
+}
+
+#endif //BOOST_REAL_REAL_HELPERS_HPP

34ac759f128cd2ffb5d684c5ff7a5a0e54025360
kishanshukla-2307 <2307kishanshukla@gmail.com>
2020-07-15 21:06:59 +0530

    implemented chudnovsky algorithm for pi

--- include/real/irrationals.hpp
+++ include/real/irrationals.hpp
@@ -15,6 +15,9 @@ namespace boost {
              * In this function, a binary base is used.
              */
             boost::real::real CHAMPERNOWNE_BINARY(boost::real::irrational::champernowne_binary_get_nth_digit, 0);
+
+            template <typename T = int>
+            boost::real::real<T> PI(boost::real::irrational::pi_nth_digit<T>, 1);
         }
     }
 }
